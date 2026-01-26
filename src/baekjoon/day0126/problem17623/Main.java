9ae3ba5d44fd04665db7dfcb19ce1e3e04c2722c
jyt001532 <jyt001532@gmail.com>
2026-01-26 14:49:15 +0900

    Add : 백준 21736 문제 solve

---
+++ src/baekjoon/day0126/problem17623/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.day0126.problem17623;
+import java.util.*;
+import java.io.*;
+public class Main {
+	/*
+	 * F(N) = F(N-1) 
+	 * DP
+	 * for i to [0..N]:
+	 * 		dp[i] = dp[i-1]+1; 
+	 * 		if(
+	 */
+	public static void main(String[] args) throws Throwable{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(st.nextToken());
+		int [] dp = new int[n+1];
+		Arrays.fill(dp,Integer.MAX_VALUE);
+		dp[0]=0;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j*j<=i;j++) {
+				dp[i] = Math.min(dp[i],dp[i-j*j]+1);
+			}
+		}
+		sb.append(dp[n]);
+		System.out.println(sb);
+	}
+
+}
--- src/baekjoon/day0126/problem24883/Main.java
+++ src/baekjoon/day0126/problem24883/Main.java
@@ -2,5 +2,17 @@ package baekjoon.day0126.problem24883;
 import java.io.*;
 import java.util.*;
 public class Main {
+	public static void main(String []args) throws IOException{
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	StringBuilder sb =new StringBuilder();
+	
+	char ch = st.nextToken().charAt(0);
+	if(ch == 'N' || ch =='n') {
+		sb.append("Naver D2");
+	}
+	else sb.append("Naver Whale");
+	
+	System.out.println(sb);
+	}
 }

8886197ab66dd93b817b65f7891c568ff89e3f60
moonjs1011 <moonjs1011@naver.com>
2026-01-27 17:55:55 +0900

    Add : 백준 2442/2443 코드 psuh

---
+++ src/baekjoon/day0127/problem2443/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.day0127.problem2443;
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=i-1;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*N-(2*i-1);j++) {
+				System.out.print("*");
+			}
+			System.out.println();g
+		}
+	}
+
+}

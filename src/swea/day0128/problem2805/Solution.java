04882d309bfce4828fa534f253b095fd760a9e64
moonjs1011 <moonjs1011@naver.com>
2026-01-29 14:44:55 +0900

    Fix : SWEA2805 문제 리팩토링 [3가지 방법 구현, 반복,DFS,BFS]

--- src/swea/day0128/problem2805/Solution.java
+++ src/swea/day0128/problem2805/Solution.java
@@ -4,27 +4,82 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int[][] matrix;
-	static int N;
+	static int[][] matrix; // 공통 전역변수
+	static int N; // 공통 전역 변수
+	static int[] dy = { -1, 0, 1, 0 }; // BFS + DFS용 전역 변수
+	static int[] dx = { 0, -1, 0, 1 }; // BFS + DFS용 전역 변수
+	static boolean[][] visited; // BFS + DFS용 전역 변수
+	static int sumDfs; // DFS용 전역 변수
 
 	static int iterator() {
 		int sum = 0;
+		int middle = N / 2;
 		for (int y = 0; y < N; y++) {
-			if (y < N / 2) { // 상반신
-				for (int x = N / 2 - y; x <= N / 2 + y; x++) {
+			if (y < middle) { // 상반부
+				for (int x = middle - y; x <= middle + y; x++) {
 					sum += matrix[y][x];
 				}
-			} else if (y == N / 2) {
+			} else if (y == middle) {// 중단부
 				for (int x = 0; x < N; x++) {
 					sum += matrix[y][x];
 				}
-			} else if (y > N / 2) {
-				for (int x = y - N / 2; x <= y + N / 2; x++) {
+			} else if (y > middle) {// 하단부, x의 끝점은 대칭을 이용하여 N-1-(시작점)
+				for (int x = y - middle; x <= N - 1 - (y - middle); x++) {
 					sum += matrix[y][x];
 				}
 			}
 		}
 		return sum;
+	}
+
+	static int bfs() {
+		// *********초기화 phase*********//
+		int middle = N / 2;
+		int sum = matrix[middle][middle];
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		visited = new boolean[N][N];
+		// ***************************//
+		// *********BFS phase*********//
+		visited[middle][middle] = true;
+		q.offer(new int[] { middle, middle });
+		for (int level = 0; level < middle; level++) {//중간으로부터 거리(계층)
+			int size = q.size(); // 큐에 존재하는 노드의 개수만큼 반복문 수행
+			for (int i = 0; i < size; i++) {
+				int[] cyx = q.poll();
+				int cy = cyx[0];
+				int cx = cyx[1];
+				for (int d = 0; d < 4; d++) {
+					int ny = cy + dy[d];
+					int nx = cx + dx[d];
+					if (ny >= 0 && ny < N && nx >= 0 && nx < N && !visited[ny][nx]) {
+						visited[ny][nx] = true;
+						sum += matrix[ny][nx];
+						q.offer(new int[] { ny, nx });
+					}
+				}
+			}
+		}
+		// ******************//
+		return sum; // 정답 리턴
+	}
+
+	static void dfs(int y, int x) {
+		if (visited[y][x])
+			return;
+		visited[y][x] = true;
+		sumDfs+= matrix[y][x];
+		for (int d = 0; d < 4; d++) {
+			int ny = y + dy[d];
+			int nx = x + dx[d];
+			int middle = N / 2;
+			if (ny >= 0 && ny < N && nx >= 0 && nx < N) {
+				//다음 (y좌표 - 중심 좌표)의 절대값 + 다음 (x좌표 - 중심 좌표)의 절대값 <= 중심 좌표
+				if (Math.abs(ny - middle) + Math.abs(nx - middle) <= middle) {
+					dfs(ny, nx);
+				}
+			}
+		}
+
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -40,19 +95,34 @@ public class Solution {
 			// -----------------Input Phase-------------//
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken()); // N 입력 받기
-			matrix = new int[N][N]; // matrix 할당
+			matrix = new int[N][N]; // matrix 초기화
+			visited = new boolean[N][N];//visited 초기화
 
-			st = new StringTokenizer(br.readLine());
-			String line = st.nextToken(); // 해당 문제는 공백 없이 입력을 받기 때문
 			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine()); // 한개의 String으로 입력을 받음
+				String line = st.nextToken();
 				for (int j = 0; j < N; j++) {
-					matrix[i][j] = line.charAt(j);
+					matrix[i][j] = line.charAt(j) - '0';
 				}
 			}
 			// -----------Solution-------------//
-			int result = iterator();
-			sb.append("#").append(" ").append(tc).append("\n");
+			int result;
+			
+			result = iterator(); // 반복문 풀이법
+			
+			// result = bfs(); //BFS 풀이법
+			
+			//DFS 풀이
+			
+//			  sumDfs =0; 
+//			  dfs(N/2,N/2); 
+//			  result = sumDfs;
+			
+			// --------------------------------//
+			// print
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb);
+		br.close();
 	}
 }

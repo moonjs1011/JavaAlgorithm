c619f520819516cbf58f69c78ccf38055678e4df
sung_1 <moonjs1011@naver.com>
2026-01-31 22:04:01 +0900

    백준 1026문제 해결

--- src/baekjoon/day0128/problem23082/Main.java
+++ src/baekjoon/day0128/problem23082/Main.java
@@ -3,9 +3,6 @@ import java.util.*;
 
 public class Main {
 	static String s;
-	static sol(int n) {
-		
-	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();

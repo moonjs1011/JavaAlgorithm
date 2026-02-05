095c0c660fc1a32c7a76ba4ddb3797c17619c8d9
moonjs1011 <moonjs1011@naver.com>
2026-02-05 17:43:57 +0900

    Add : 백준 0205 문제 제출

---
+++ src/baekjoon/day0205/problem15649/Main.java
@@ -0,0 +1,46 @@
+package baekjoon.day0205.problem15649;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N,M;
+	static int[] arr,target;
+	static boolean[] visited;
+	static StringBuilder sb =new StringBuilder();
+	static void perm(int depth) {
+		if(depth==M) {
+			for(int ele : target)
+				sb.append(ele+" ");
+			sb.append("\n");
+			return;
+		}
+		for(int i=0;i<N;i++) {
+			if(!visited[i]) {
+				visited[i]=true;
+				target[depth] = arr[i];
+				perm(depth+1);
+				visited[i]=false;
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		visited = new boolean[N];
+		target = new int[M];
+		
+		for(int i=0;i<N;i++)
+			arr[i]=i+1;
+		perm(0);
+		
+		System.out.println(sb);
+		
+	}
+
+}

caa64b4455b4e9b19d5f9d83a9cee2c000fa4c42
moonjs1011 <moonjs1011@naver.com>
2026-02-19 16:18:24 +0900

    Fix : SWEA 1247 주석 추가

--- src/swea/month02/week02/day0213/problem1247/Solution.java
+++ src/swea/month02/week02/day0213/problem1247/Solution.java
@@ -18,20 +18,22 @@ public class Solution {
 	static Point[] customer, target;
 	static Point work,home;
 	static boolean[] visited;
-	static int minDist;
+	static int minDist;//출력할 정답
 
-	static int caluate() {
+	static int calculate() {
+		//회사와 고객들의 집 리스트 중 첫번째 좌표의 거리를 계산
 		int dist = Math.abs(work.x- target[0].x) + Math.abs(work.y - target[0].y);
 		for (int i = 0; i < N - 1; i++) {
 			dist += Math.abs(target[i].x - target[i + 1].x) + Math.abs(target[i].y - target[i + 1].y);
 		}
+		//집과 고객들의 집 리스트 중 마지막 좌표의 거리를 계산
 		dist += Math.abs(target[N - 1].x - home.x) + Math.abs(target[N - 1].y - home.y);
 		return dist;
 	}
 
 	static void perm(int depth) {
 		if (depth == N) {
-			int dist = caluate();
+			int dist = calculate(); 
 			minDist = Math.min(minDist, dist);
 			return;
 		}
@@ -59,8 +61,10 @@ public class Solution {
 			visited =new boolean[N];
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
+			//회사 좌표 입력
 			int workX = Integer.parseInt(st.nextToken());
 			int workY = Integer.parseInt(st.nextToken());
+			//집 좌표 입력
 			int homeX = Integer.parseInt(st.nextToken());
 			int homeY = Integer.parseInt(st.nextToken());
 			
@@ -68,6 +72,7 @@ public class Solution {
 			home = new Point(homeX,homeY);
 			
 			for (int i = 0; i < N; i++) {
+				//들려야하는 집들의 좌표 입력
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
 				

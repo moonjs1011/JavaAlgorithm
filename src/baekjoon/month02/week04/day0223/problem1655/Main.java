f84404f517c6357c698efe57ab2ed67683c1a6b7
sung_1 <moonjs1011@naver.com>
2026-02-23 21:44:08 +0900

    Add : 백준 1655 문제 풀이 중, 다시 풀것

---
+++ src/baekjoon/month02/week04/day0223/problem1655/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.month02.week04.day0223.problem1655;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ * q의 상태
+ * 1 ->1
+ * 1,5 ->1
+ * 1 2 5 -> 2
+ * 1 2 5 10 ->2
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+      문제 다시 풀어라
+    }
+}

74847183ab9df36f3dd4748a05a2950f97ee5ec7
moonjs1011 <moonjs1011@naver.com>
2026-03-13 15:46:01 +0900

    Add : SWEA 7793 코드 제출(다시 풀어볼 것)

--- src/swea/month03/week02/day0313/problem7793/Solution.java
+++ src/swea/month03/week02/day0313/problem7793/Solution.java
@@ -26,58 +26,12 @@ public class Solution {
 	static int[] dx = { 0, -1, 0, 1 };
 
 	static int N, M;
-	static int startY, startX, dstY, dstX, demonY, demonX;
+	static int startY, startX, dstY, dstX;
+	static Queue<int[]> q1,q2;
 
 	static boolean rangeCheck(int y, int x) {
 		return y >= 0 && y < N && x >= 0 && x < M;
 	}
-
-	static int[][] bfs() {
-		int[][] dist = new int[N][M];
-		for (int i = 0; i < N; i++)
-			Arrays.fill(dist[i], Integer.MAX_VALUE);
-		dist[startY][startX] = 0;
-		Queue<int[]> q1 = new ArrayDeque<>();
-		Queue<int[]> q2 = new ArrayDeque<>();
-		q1.offer(new int[] { startY, startX });// {y,x, option(공주인지 악마인지)
-		q2.offer(new int[] { demonY, demonX });
-		while (!q1.isEmpty()) {
-			int[] info = q1.poll();
-			int cy = info[0];
-			int cx = info[1];
-			for (int d = 0; d < 4; d++) {
-				int ny = cy + dy[d];
-				int nx = cx + dx[d];
-				if (rangeCheck(ny, nx) && grid[ny][nx].ch == '.' && !grid[ny][nx].isPolluate
-						&& dist[ny][nx] > dist[cy][cx] + 1) {
-					// 공주 이동	
-					grid[cy][cx].ch = '.';
-					grid[ny][nx].ch = 'S';
-					// 거리 갱신
-					dist[ny][nx] = dist[cy][cx] + 1;
-					// 큐 삽입
-					q1.offer(new int[] { ny, nx });
-				}
-			}
-			info = q2.poll();
-			cy = info[0];
-			cx = info[1];
-			for (int d = 0; d < 4; d++) {
-				int ny = cy + dy[d];
-				int nx = cx + dx[d];
-				if (rangeCheck(ny, nx) && grid[ny][nx].ch == '.') {
-					grid[ny][nx].ch = '*';
-					grid[ny][nx].isPolluate = true;
-					q2.offer(new int[] { ny, nx });
-				}
-			}
-
-		}
-
-		return dist;
-
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
@@ -85,6 +39,9 @@ public class Solution {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
+			
+			q1 = new ArrayDeque<>();
+			q2 = new ArrayDeque<>();
 			grid = new Point[N][M];
 			for (int i = 0; i < N; i++) {
 				String line = br.readLine();
@@ -97,20 +54,64 @@ public class Solution {
 						dstY = i;
 						dstX = j;
 					} else if (grid[i][j].ch == '*') {
-						demonY = i;
-						demonX = j;
+						q2.offer(new int[] {i,j});
 					}
 				}
 			}
-			for (Point[] P : grid) {
-				for (Point p : P) {
-					System.out.print(p.ch);
-				}
-				System.out.println();
-			}
-			int dist[][] = bfs();
-			System.out.println(dist[dstY][dstX]);
+		
+			int time = bfs();
+			if(time!=-1)
+				System.out.println("#"+tc+" "+time);
+			else System.out.println("#"+tc+" GAME OVER");
 		}
+	
+	}
+	/*
+	 * 최초 수연위치를 수연큐에 넣음
+	 * 최초 악마위치를 악마큐에 넣음
+	 * 악마큐에 있는 모든 정보를 하나씩 꺼내가면서 
+	 * 1.grid를 *로 마스킹 후 다음 악마 좌표 악마큐에 삽입
+	 * 수연 큐에 있는 정보를 하나꺼내서 1만큼이동
+	 */
+	static int bfs() {
+		q1.offer(new int[] { startY, startX });// {y,x, option(공주인지 악마인지)
+		boolean[][]visited = new boolean[N][M];
+		visited[startY][startX]=true;
+		int time=0;
+		while (!q1.isEmpty()) {
+			time+=1;
+			int q2Size = q2.size();
+			for(int iter =0; iter<q2Size;iter++) {
+				int[]info = q2.poll();
+				int cy = info[0];
+				int cx = info[1];
+				for(int i=0;i<4;i++) {
+					int ny = cy + dy[i];
+					int nx = cx + dx[i];
+					if(rangeCheck(ny, nx)&&(grid[ny][nx].ch=='.'||grid[ny][nx].ch=='S')) {
+						grid[ny][nx].ch = '*';
+						q2.offer(new int[] {ny,nx});
+					}
+				}
+			}
+			int q1Size = q1.size();
+			for(int iter =0; iter<q1Size;iter++) {
+				int[]info = q1.poll();
+				int cy = info[0];
+				int cx = info[1];
+				if(grid[cy][cx].ch=='D') return time-1;
+				for(int i=0;i<4;i++) {
+					int ny = cy + dy[i];
+					int nx = cx + dx[i];
+					if(rangeCheck(ny, nx)&&!visited[ny][nx]&&(grid[ny][nx].ch=='.'||grid[ny][nx].ch=='D')) {
+						visited[ny][nx] =true;
+						q1.offer(new int[] {ny,nx});
+					}
+				}
+			}
+		}
+
+		return -1;
 
 	}
 }

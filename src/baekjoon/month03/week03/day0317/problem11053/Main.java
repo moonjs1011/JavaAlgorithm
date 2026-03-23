e86d1d4dde44837003163d16ef54cf3195dc8b84
sung_1 <moonjs1011@naver.com>
2026-03-23 22:58:03 +0900

    Add : 백준 11053 제출

--- src/baekjoon/month03/week03/day0317/problem11053/Main.java
+++ src/baekjoon/month03/week03/day0317/problem11053/Main.java
@@ -2,32 +2,29 @@ package baekjoon.month03.week03.day0317.problem11053;
 
 import java.io.*;
 import java.util.StringTokenizer;
+
 public class Main {
-	static int[] arr;
-	static boolean[][] isCached;
-	static int[][] cached;
-	static int LSS(int i,int j) {
-		if(i == arr.length||j==arr.length) return 0;
-		
-		if(!isCached[i][j]) {
-			isCached[i][j]=true;
-			if(arr[i]<arr[j]) cached[i][j]= 1 + LSS(i+1,j+1);
-			else cached[i][j]=Math.max(LSS(i+1,j),LSS(i,j+1));
-		}
-		return cached[i][j];
-	}
-	public static void main(String[] args) throws IOException{
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		arr = new int[N];
-		isCached =new boolean[N][N];
-		cached =new int[N][N];
-		StringTokenizer st =new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int res = LSS(0,0);
-		System.out.println(res);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            dp[i]=1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
 }
